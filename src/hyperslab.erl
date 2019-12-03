9d3bb9f042ddc0225fb033ea7408ebff212f230a
44044 <zheng.yao@seniverse.com>
2019-12-03 17:30:53 +0800

    get hyperslab of each tile

--- src/hyperslab.erl
+++ src/hyperslab.erl
@@ -1,12 +1,20 @@
 -module(hyperslab).
 
--export([tiles/1, tiles/2, subsets/2, validate/2]).
+-export([tiles/1, validate/2, subslabs/2, parts/3, subsets/2]).
 
 -type dimension() :: {pos_integer(), pos_integer()}.
 
 -type tile() :: [non_neg_integer()].
 
 -type part() :: {non_neg_integer(), pos_integer()}.
+
+-type index() :: non_neg_integer()
+               | #{first => non_neg_integer(),
+                   step => pos_integer(),
+                   last => non_neg_integer()}.
+
+-type hyperslab() :: [index()].
+
 
 -spec tiles([dimension()]) -> [{tile(), [part()]}].
 
@@ -50,77 +58,100 @@ parts(Base, Parts) ->
     [{Base + Offset, Size} || {Offset, Size} <- Parts].
 
 
--spec validate([index()], [dimension()]) -> boolean().
+-spec validate(hyperslab(), [dimension()]) -> boolean().
 
 validate([], []) ->
     true;
 validate([N|T1], [{Size, _}|T2]) when is_integer(N) ->
     (0 =< N) and (N < Size) and validate(T1, T2);
 validate([Index|T1], [{Size, _}|T2]) when is_map(Index) ->
+    {First, Last, Step} = normalize_index(Index, Size),
+    Max = First + (Last - First) div Step * Step,
+    (0 =< First) and (First =< Max) and (Max < Size) and validate(T1, T2).
+
+normalize_index(Index, _Size) when is_integer(Index) ->
+    {Index, Index, 1};
+normalize_index(Index, Size) when is_map(Index) ->
     First = maps:get(first, Index, 0),
     Last = maps:get(last, Index, Size-1),
     Step = maps:get(step, Index, 1),
-    Max = First + (Last - First) div Step * Step,
-    (0 =< First) and (First < Size) and (0 =< Max) and (Max < Size) and validate(T1, T2).
+    {First, Last, Step}.
 
 
--type index() :: non_neg_integer()
-               | #{first => non_neg_integer(),
-                   step => pos_integer(),
-                   last => non_neg_integer()}.
+-spec subslabs(hyperslab(), [dimension()]) -> [{tile(), hyperslab()}].
+subslabs(Hyperslab, Dimensions) ->
+    subslabs(Hyperslab, Dimensions, fun slab/3).
 
--spec tiles([index()], [dimension()]) -> [tile()].
-tiles([H1], [H2]) ->
-    [[N] || {N, _} <- slice(H1,H2)];
-tiles([H1|T1], [H2|T2]) ->
-    [[H|T]
-     || {H, _} <- slice(H1, H2),
-        T <- tiles(T1, T2)].
+-spec parts(tile(), hyperslab(), [dimension()]) -> [part()].
+parts(Tile, Hyperslab, Dimensions) ->
+    {_, Parts} = slices(Tile, Hyperslab, Dimensions),
+    Parts.
 
--spec subsets([index()], [dimension()]) -> [{tile(), [part()]}].
+slices([H1], [H2], [H3]) ->
+    Size = tile_size(H1, H3),
+    case normalize_index(H2, Size) of
+        {First, Last, 1} ->
+            {Size, [{First, Last - First + 1}]};
+        {First, Last, Step} ->
+            {Size, [{I, 1} || I <- lists:seq(First, Last, Step)]}
+    end;
+slices([H1|T1], [H2|T2], [H3|T3]) ->
+    {Acc, Parts} = slices(T1, T2, T3),
+    Size = tile_size(H1, H3),
+    {First, Last, Step} = normalize_index(H2, Size),
+    {Acc * Size,
+     [{I * Acc + Offset, Length}
+      || I <- lists:seq(First, Last, Step),
+         {Offset, Length} <- Parts]}.
+
+-spec subsets(hyperslab(), [dimension()]) -> [{tile(), part()}].
 
 subsets(Hyperslab, Dimensions) ->
-    [{Tile, Parts} || {Tile, _, Parts} <- slices(Hyperslab, Dimensions)].
+    [{Tile, Part} || {Tile, _, Part} <- slices(Hyperslab, Dimensions)].
 
-slices([H1], [H2]) ->
-    [{[N], tile_size(N, H2), Parts} || {N, Parts} <- slice(H1,H2)];
-slices([H1|T1], [H2|T2]) ->
+slices([Index], [{Size, Tile}]) ->
+    Subslabs = subslabs(Index, Size, Tile, fun slab_size/3),
+    [{[N], S, {0, S}} || {N, S} <- Subslabs ];
+slices([Index|T1], [{Size, Tile}|T2]) ->
     Slices = slices(T1, T2),
-    [Slice
-     || {H, Parts} <- slice(H1,H2),
-        {Offset, Length} <- Parts,
-        I <- lists:seq(Offset, Offset+Length-1),
-        Slice <- slices(H, I, tile_size(H, H2), Slices)].
-
-slices(H, I, Tile, Slices) ->
-    [ {[H|T], Tile * Size, parts(I * Size, Parts)}
-      || {T, Size, Parts} <- Slices].
+    Subslabs = subslabs(Index, Size, Tile, fun slab_size/3),
+    [{[H|T], S * A, {I * A + Offset, Length}}
+     || {H, S} <- Subslabs,
+        I <- lists:seq(0, S-1),
+        {T, A, {Offset, Length}} <- Slices].
 
 
-slice(N, {_, Tile}) when is_integer(N) ->
-    [{N - N rem Tile, [{N rem Tile, 1}]}];
-slice(Index, {Size, Tile}) ->
-    First = maps:get(first, Index, 0),
-    Last = maps:get(last, Index, Size-1),
-    Step = maps:get(step, Index, 1),
-    slice(First, Last, Step, Tile).
+subslabs([], [], _) ->
+    [{[], []}];
+subslabs([Index|T1], [{Size, Tile}|T2], Fun) ->
+    H = subslabs(Index, Size, Tile, Fun),
+    T = subslabs(T1, T2, Fun),
+    [ {[H3|T3], [H4|T4]}
+      || {H3,H4} <- H,
+         {T3,T4} <- T ].
 
-slice(First, Last, 1, Tile) when First - First rem Tile < Last - Last rem Tile ->
+subslabs(Index, Size, Tile, Fun) ->
+    {First, Last, Step} = normalize_index(Index, Size),
+    subslabs(First, First + (Last - First) div Step * Step, Step, Tile, Fun).
+
+subslabs(First, Last, Step, Tile, Fun) when First - First rem Tile < Last - Last rem Tile ->
     Offset = First rem Tile,
     Base = First - Offset,
-    [{Base, [{Offset, Tile - Offset}]}|slice(Base + Tile, Last, 1, Tile)];
-slice(First, Last, 1, Tile) when First =< Last ->
+    Max = Offset + (Tile - 1 - Offset) div Step * Step,
+    [{Base, Fun(Offset, Max, Step)}|subslabs(Base + Max + Step, Last, Step, Tile, Fun)];
+subslabs(First, Last, Step, Tile, Fun) when First =< Last ->
     Offset = First rem Tile,
-    [{First - Offset, [{Offset, Last + 1 - First}]}];
-slice(First, Last, Step, Tile) ->
-    merge([{N - N rem Tile, {N rem Tile, 1}} || N <- lists:seq(First, Last, Step)]).
+    Base = First - Offset,
+    [{Base, Fun(Offset, Last rem Tile, Step)}];
+subslabs(_First, _Last, _Step, _Tile, _Fun) ->
+    [].
 
-merge([]) ->
-    [];
-merge([{Tile, Offset}|T]) ->
-    case merge(T) of
-        [{Tile, Offsets}|Rest] ->
-            [{Tile, [Offset|Offsets]}|Rest];
-        Rest ->
-            [{Tile, [Offset]}|Rest]
-    end.
+slab(First, First, _Step) ->
+    First;
+slab(First, Last, 1) ->
+    #{first => First, last => Last};
+slab(First, Last, Step) ->
+    #{first => First, last => Last, step => Step}.
+
+slab_size(First, Last, Step) ->
+    (Last - First) div Step + 1.
--- test/hyperslab_SUITE.erl
+++ test/hyperslab_SUITE.erl
@@ -4,7 +4,7 @@
 -compile(export_all).
 
 all() ->
-    [test_tiles, test_tiles2, test_subsets, test_validate, test_all].
+    [test_tiles, test_validate, test_subslabs, test_subsets, test_all].
 
 test_tiles(_Config) ->
     [{[0,0],[{0,2},{4,2}]},
@@ -17,59 +17,6 @@ test_tiles(_Config) ->
      {[2,0],[{6,2}]},
      {[2,2],[{8,1}]}] = hyperslab:tiles([{3, 2}, {3, 2}]).
 
-test_tiles2(_Config) ->
-    [[0,0],[0,2],[2,0],[2,2]] =
-        hyperslab:tiles(
-          [#{},#{}],
-          [{4,2}, {4,2}]).
-
-test_subsets(_Config) ->
-    [{[0,0], [{0,2}]},
-     {[0,2], [{0,2}]},
-     {[0,0], [{2,2}]},
-     {[0,2], [{2,2}]},
-     {[2,0], [{0,2}]},
-     {[2,2], [{0,2}]},
-     {[2,0], [{2,2}]},
-     {[2,2], [{2,2}]}] =
-        hyperslab:subsets(
-          [#{},#{}],
-          [{4,2}, {4,2}]),
-
-    [{[0,0], [{0,2}]},
-     {[0,2], [{0,1}]},
-     {[0,0], [{2,2}]},
-     {[0,2], [{1,1}]},
-     {[2,0], [{0,2}]},
-     {[2,2], [{0,1}]}] =
-        hyperslab:subsets(
-          [#{},#{}],
-          [{3,2}, {3,2}]),
-
-    [{[0,0], [{3,1}]}] =
-        hyperslab:subsets(
-          [1,1],
-          [{4,2}, {4,2}]),
-
-    [{[0,0],[{0,1}]},
-     {[0,2],[{0,1}]},
-     {[2,0],[{0,1}]},
-     {[2,2],[{0,1}]}] =
-        hyperslab:subsets(
-          [#{step => 2}, #{step => 2}],
-          [{4,2}, {4,2}]),
-
-    [{[0,0],[{0,1},{2,1}]},
-     {[0,3],[{1,1}]},
-     {[0,0],[{6,1},{8,1}]},
-     {[0,3],[{5,1}]},
-     {[3,0],[{3,1},{5,1}]},
-     {[3,3],[{3,1}]}] =
-        hyperslab:subsets(
-          [#{step => 2}, #{step => 2}],
-          [{5,3}, {5,3}]).
-
-
 test_validate(_Config) ->
     true = hyperslab:validate([1], [{2, 2}]),
     false = hyperslab:validate([2], [{2, 2}]),
@@ -79,28 +26,129 @@ test_validate(_Config) ->
     false = hyperslab:validate([#{first=>1, step=>2, last=>3}], [{2, 2}]),
     false = hyperslab:validate([#{first=>1, step=>2, last=>6}], [{5, 2}]).
 
+test_subslabs(_Config) ->
+    [{[0,0],[#{first := 0,last := 1},#{first := 0,last := 1}]},
+     {[0,2],[#{first := 0,last := 1},#{first := 0,last := 1}]},
+     {[2,0],[#{first := 0,last := 1},#{first := 0,last := 1}]},
+     {[2,2],[#{first := 0,last := 1},#{first := 0,last := 1}]}] =
+        hyperslab:subslabs(
+          [#{},#{}],
+          [{4,2}, {4,2}]),
 
-make_data(Dimensions) ->
-    Size = lists:foldl(fun({S, _}, Acc) -> S * Acc end, 1, Dimensions),
+    [{[0,0],[#{first := 0,last := 1},#{first := 0,last := 1}]},
+     {[0,2],[#{first := 0,last := 1},0]},
+     {[2,0],[0,#{first := 0,last := 1}]},
+     {[2,2],[0,0]}] =
+        hyperslab:subslabs(
+          [#{},#{}],
+          [{3,2}, {3,2}]),
+
+    [{[0,0],[1,1]}] =
+        hyperslab:subslabs(
+          [1,1],
+          [{4,2}, {4,2}]),
+
+    [{[0,0],[0,0]},
+     {[0,2],[0,0]},
+     {[2,0],[0,0]},
+     {[2,2],[0,0]}] =
+        hyperslab:subslabs(
+          [#{step => 2}, #{step => 2}],
+          [{4,2}, {4,2}]),
+
+    [{[0,0],
+      [#{first := 0,last := 2,step := 2},
+       #{first := 0,last := 2,step := 2}]},
+     {[0,3],[#{first := 0,last := 2,step := 2},1]},
+     {[3,0],[1,#{first := 0,last := 2,step := 2}]},
+     {[3,3],[1,1]}] =
+        hyperslab:subslabs(
+          [#{step => 2}, #{step => 2}],
+          [{5,3}, {5,3}]).
+
+test_subsets(_Config) ->
+    [{[0,0], {0,2}},
+     {[0,2], {0,2}},
+     {[0,0], {2,2}},
+     {[0,2], {2,2}},
+     {[2,0], {0,2}},
+     {[2,2], {0,2}},
+     {[2,0], {2,2}},
+     {[2,2], {2,2}}] =
+        hyperslab:subsets(
+          [#{},#{}],
+          [{4,2}, {4,2}]),
+
+    [{[0,0], {0,2}},
+     {[0,2], {0,1}},
+     {[0,0], {2,2}},
+     {[0,2], {1,1}},
+     {[2,0], {0,2}},
+     {[2,2], {0,1}}] =
+        hyperslab:subsets(
+          [#{},#{}],
+          [{3,2}, {3,2}]),
+
+    [{[0,0], {0,1}}] =
+        hyperslab:subsets(
+          [1,1],
+          [{4,2}, {4,2}]),
+
+    [{[0,0],{0,1}},
+     {[0,2],{0,1}},
+     {[2,0],{0,1}},
+     {[2,2],{0,1}}] =
+        hyperslab:subsets(
+          [#{step => 2}, #{step => 2}],
+          [{4,2}, {4,2}]),
+
+    [{[0,0],{0,2}},
+     {[0,3],{0,1}},
+     {[0,0],{2,2}},
+     {[0,3],{1,1}},
+     {[3,0],{0,2}},
+     {[3,3],{0,1}}] =
+        hyperslab:subsets(
+          [#{step => 2}, #{step => 2}],
+          [{5,3}, {5,3}]).
+
+
+make_data(Sizes) ->
+    Size = lists:foldl(fun(S, Acc) -> S * Acc end, 1, Sizes),
     << <<(rand:uniform(256)-1)>> || _ <- lists:seq(1, Size) >>.
 
 extract(Subsets, Tiles) ->
     << <<(binary:part(maps:get(Tile, Tiles), Part))/binary>>
-       || {Tile, Parts} <- Subsets,
-          Part <- Parts >>.
+       || {Tile, Part} <- Subsets>>.
+
+extract(Data, Hyperslab, Dimensions) ->
+    Tiles =
+        maps:from_list(
+          [{Tile, << <<(binary:part(Data, Part))/binary>> || Part <- Parts >>}
+           || {Tile, Parts} <- hyperslab:tiles(Dimensions)]),
+
+    Subslabs = hyperslab:subslabs(Hyperslab, Dimensions),
+
+    Tiles1 =
+        maps:from_list(
+          [ {Tile,
+             << <<(binary:part(maps:get(Tile, Tiles), Part))/binary>>
+                || Part <- hyperslab:parts(Tile, Slab, Dimensions) >>}
+            || {Tile, Slab} <- Subslabs ]),
+
+    extract(hyperslab:subsets(Hyperslab, Dimensions), Tiles1).
+
 
 test_tile(Hyperslab, Dimensions) ->
-    Data = make_data(Dimensions),
-    Tiles = maps:from_list(
-              [{Tile, << <<(binary:part(Data, Part))/binary>> || Part <- Parts >>}
-               || {Tile, Parts} <- hyperslab:tiles(Dimensions)]),
-    Expected =
-        extract(
-          hyperslab:subsets(Hyperslab, [{Size, Size} || {Size, _} <- Dimensions ]),
-          #{[0 || _ <- Dimensions] => Data}),
-    Expected = extract(hyperslab:subsets(Hyperslab, Dimensions), Tiles).
+    {Sizes, _} = lists:unzip(Dimensions),
+    Data = make_data(Sizes),
+    Expected = extract(Data, Hyperslab, lists:zip(Sizes, Sizes)),
+    Expected = extract(Data, Hyperslab, Dimensions),
+    ok.
 
 test_all(_Config) ->
     test_tile([5], [{10,3}]),
     test_tile([#{}, #{}], [{5,3},{5,4}]),
-    test_tile([#{first=>1, step=>3, last=>17}, #{first=>3, step=>7, last=>18}], [{20,7},{20,3}]).
+    test_tile([#{first=>1, step=>3, last=>17}, #{first=>3, step=>7, last=>18}], [{20,7},{20,3}]),
+    test_tile([#{first=>1, step=>3, last=>17}, #{first=>3, step=>7, last=>18}, #{first=>1, step=>3, last=>17}, #{first=>3, step=>7, last=>18}], [{20,7},{20,3},{20,7},{20,3}]),
+    ok.
